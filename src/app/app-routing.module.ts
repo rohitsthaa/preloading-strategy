2da9ef27db34f5df6b72e93983639fc7f4fff618
Rohit Shrestha <srohitiw@gmail.com>
2019-04-01 16:50:36 +0545

    created email detail page, added path to the app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmailListComponent } from './email-list/email-list.component';
+import { EmailDetailComponent } from './email-detail/email-detail.component';
 
 const routes: Routes = [
   {
     path: '',
     component: EmailListComponent
+  },
+  {
+    path: 'detail',
+    component: EmailDetailComponent
   }
 ];
 
