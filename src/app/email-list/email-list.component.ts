c6fbdff74988b6133dde914347ed3572256a72b5
Rohit Shrestha <srohitiw@gmail.com>
2019-04-01 16:44:26 +0545

    show list of email in email list component

--- src/app/email-list/email-list.component.ts
+++ src/app/email-list/email-list.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailListComponent implements OnInit {
 
+  listofEmail = [
+    {
+      'sender': 'Rohit Shrestha',
+      'subject': 'Application for leave',
+      'description' : 'asdasdasda.........'
+    },
+    {
+      'sender': 'Hilson Shrestha',
+      'subject': 'Application for test',
+      'description' : 'asdasdasda.........'
+    },
+    {
+      'sender': 'Aatish Shrestha',
+      'subject': 'Application for account',
+      'description' : 'asdasdasda.........'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
